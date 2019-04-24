be1ce95706a6b7e9e57fb47d556393e005542f0d
Gaziz Sattarov <gaziz.sattarov@gmail.com>
2019-04-24 17:50:25 +0600

    migrations changed

--- src/main/resources/db/migration/V1_1__withoutTime.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE task (
-  id varchar PRIMARY KEY,
-  text varchar NOT NULL,
-  status varchar NOT NULL
-);
--- src/main/resources/db/migration/V1_3__updatedAt.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE task_V2(
-  id varchar PRIMARY KEY,
-  text varchar NOT NULL,
-  status varchar NOT NULL,
-  createdAt varchar NOT NULL,
-  updatedAt varchar NOT NULL
-);
